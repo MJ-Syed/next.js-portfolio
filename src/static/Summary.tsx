0c9af96f4b759ee05ec4a8003978fe5174fc422b
syed mujtaba abbas <asyedmujtaba7@gmail.com>
2024-11-15 21:14:47 +0500

    Add files via upload

---
+++ src/static/Summary.tsx
@@ -0,0 +1,11 @@
+
+function Summary(){
+    return (<>
+        <p className="text-4xl mb-8">About me</p>
+        <article className="desktop:text-xl mobile:text-md" itemScope itemType="https://schema.org/author">
+            <p className="mb-5">Hello! I&apos;m Syed Mujtaba Abbas, a passionate and dedicated web developer with a knack for crafting dynamic, user-centric web applications. With expertise in front-end and back-end development, I specialize in building seamless, interactive websites that deliver both functionality and aesthetic appeal. My experience spans various modern technologies, including Next.js, TypeScript, and responsive design principles, ensuring that every project I work on is optimized and adaptable across devices </p>
+        </article>
+    </>);
+}
+
+export default Summary;
